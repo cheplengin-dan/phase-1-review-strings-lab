36fceec09c74cbd9baceef0d5fb7d8fbdf2a865d
Duncan <cheplenginduncan@gmail.com>
2023-03-13 22:10:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = "Grass Hopper";
+
+const  welcomeMessage = `'Welcome to Flatbook, ${currentUser}'!`;
+
+const excitedWelcomeMessage = `"WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}"!`;
+
+const shortGreeting = "Welcome, G!";
+
+
+
